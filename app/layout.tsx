2fef24cc3a1e99512859ab5c2b9e7e2a62d651e8
ethanp <ethanp.dev@gmail.com>
2023-09-01 21:19:39 +0700

    init base source

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { SiteFooter } from "@/components/site-footer";
+import { SiteHeader } from "@/components/site-header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,9 +18,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={fontSans.variable}>
-        <div id="root">{children}</div>
+    <html lang="en" suppressHydrationWarning>
+      <head></head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative flex min-h-screen flex-col">
+            <SiteHeader />
+            <div className="flex-1">{children}</div>
+            <SiteFooter />
+          </div>
+          <TailwindIndicator />
+        </ThemeProvider>
       </body>
     </html>
   );
