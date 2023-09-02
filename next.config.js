7837e954ec18d6f6b07a678d4c6468317d26b5de
ethanp <ethanp.dev@gmail.com>
2023-09-02 18:19:58 +0700

    Avoid AWS SDK Node.js require issue

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "mir-s3-cdn-cf.behance.net"],
   },
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === "nodejs") {
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+      );
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
